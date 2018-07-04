72d01575b63fbefb2ff4d7dbb1796e6e9a1d3806
root <github@eos.store>
2018-07-04 11:47:02 -0400

    add file

---
+++ fullnode/cleos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#docker exec eosio-nodeos sh -c "cleos -u http://127.0.0.1:8886 $*"
+sh -c "cleos -u http://127.0.0.1:8889 $*"
+
---
+++ test/start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+kill `cat nodeos.pid`
+
+rm -rf nodeos/data
+#rm -rf /root/eosio-wallet/./default.wallet
+#docker stop eosio-nodeos
+#docker rm eosio-nodeos
+#docker run -ti --detach --name eosio-nodeos -p 8889:8889 -p 9877:9877 -v `pwd`:/root/.local/share/eosio eosio/eos:v1.0.0 /opt/eosio/bin/nodeos  --genesis-json=/root/.local/share/eosio/nodeos/config/genesis.json
+
+
+nohup nodeos --data-dir `pwd`/nodeos/data --genesis-json=`pwd`/nodeos/config/genesis.json --config-dir `pwd`/nodeos/config & echo $! >nodeos.pid
